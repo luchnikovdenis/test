da34598c62308f1d07aa72ac322c33aa3669d1f3
Denis Luchnikov <denis.luchnikov.95@mail.ru>
2018-07-22 22:08:46 +0500

    Add files

---
+++ backend/1.5/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gallery</title>
+</head>
+<body>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+	<input type="file" name="file">
+	<button type="submit" name="submit">Загрузить</button>
+</form>
+
+<?php
+?>
+</body>
+</html>
